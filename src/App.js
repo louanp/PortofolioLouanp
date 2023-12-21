338db4706a9f294c64e421288e1a523eaf3acb99
louanp <louan.poirier@gmail.com>
2023-12-21 18:56:34 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
-    </div>
+    <HashRouter base="/">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h1 className="text-3xl font-bold underline">
+              Hello world!
+            </h1>
+            Learn React
+          </a>
+        </header>
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+        </h1>
+      </div>
+    </HashRouter>
   );
 }
 
